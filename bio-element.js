d93bbf6e55d3f699c49882af86e619497dcbe1d7
Sebastian Wilzbach <seb@wilzba.ch>
2015-07-10 00:44:39 +0200

    moved dependency require statements to main file

--- bio-element.js
+++ bio-element.js
@@ -1,10 +1,6 @@
 var _ = require('lodash');
 var Polymer  = require('polymer');
 
-// we can't use bio-container in the name when we use systemjs mappings
-require('./bio-element.html!html');
-//require('./bio-element.css!css');
-
 var Bioelement = Polymer({
 
   is: 'bio-element',
---
+++ main.js
@@ -0,0 +1,5 @@
+// we can't use bio-container in the name when we use systemjs mappings
+require('./bio-element.html!html');
+//require('./bio-element.css!css');
+
+module.exports = require('./bio-element');
