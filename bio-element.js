2534bcce29c9c9287a48182b25fd604cef289604
Herculano Campos <herkulano@gmail.com>
2015-07-10 16:41:09 +0100

    removed main

--- bio-element.js
+++ bio-element.js
@@ -1,17 +1,22 @@
-var Bioelement = Polymer({
+var Polymer  = require('polymer');
+
+// we can't use bio-container in the name when we use systemjs mappings
+require('./bio-element.html!html');
+
+var BioElement = Polymer({
 
   is: 'bio-element',
 
   properties: {
     text: {
       type: String,
-      value: 'Hello'
+      value: 'hello'
     }
   },
 
   created: function() {
-    console.log('bio-element created');
+    console.log('<bio-element> created');
   }
 });
 
-module.exports = Bioelement;
+module.exports = BioElement;
--- main.js
+++
@@ -1,6 +0,0 @@
-require('webcomponentsjs-lite');
-var Polymer  = require('polymer');
-
-// we can't use bio-container in the name when we use systemjs mappings
-require('./bio-element.html!html');
-module.exports = require('./bio-element');
